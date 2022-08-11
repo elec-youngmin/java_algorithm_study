38fa7f242a59bc53211167a6179cffa8fe782cfb
elec_youngmin <mintzerocode@gmail.com>
2022-08-11 20:46:29 +0900

    init

---
+++ src/chapter_1/Question_4.java
@@ -0,0 +1,29 @@
+package chapter_1;
+
+import java.util.Scanner;
+
+public class Question_4 {
+
+    void Login() {
+
+
+
+    }
+
+    public static void  main(String[] args){
+
+        Scanner in=new Scanner(System.in);
+        int count=in.nextInt();
+        int[] arr=new int[count];
+
+        for(int i=0; i<=count; i++){
+            arr[i]=in.nextInt();
+        }
+        Question_4 question_4=new Question_4();
+
+
+
+
+    }
+
+}
